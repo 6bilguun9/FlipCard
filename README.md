44d3b5b340b79944cf8a52b036cd83598a19645b
6bilguun9 <batsukhbilguun999@gmail.com>
2024-10-07 22:09:30 -0400

    added more functionality for project 3

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Web Development Project 2 - Flash Cards
+# Web Development Project 3 - Flash Cards
 
 Submitted by: Bilguun Batsukh
 
-This web app: displays flashCards with random order
+This web app: Flash Cards of Celebs
 
 Time spent: 4 hours spent in total
 
@@ -10,30 +10,28 @@ Time spent: 4 hours spent in total
 
 The following **required** functionality is completed:
 
-- [x] **The title of the card set and some information about it, such as a short description and the total number of cards are displayed**
-- [x] **A single card at a time is displayed, only showing one of the components of the information pair**
-- [x] **A list of card pairs is created**
-- [x] **Clicking on the card shows the corresponding component of the information pair**
-- [x] **Clicking the next button displays a random new card**
+- [x] **The user can enter their guess in a box before seeing the flipside of the card**
+- [x] **Clicking on a submit button shows visual feedback about whether the answer was correct or incorrect**
+- [x] **A back button is displayed on the card and can be used to return to the previous card in a set sequence**
+- [x] **A next button is displayed on the card and can be used to navigate to the next card in a set sequence**
 
 The following **optional** features are implemented:
 
-- [x] Cards contains images in addition to or in place of text
-- [x] Cards have different visual styles such as color based on their category
-  - [x] _visual style implemented_
+- [x] A shuffle button is used to randomize the order of the cards
+- [x] A user's answer may be counted as correct even when it is slightly different from the target answer
+- [x] A counter displays the user's current and longest streak of correct responses
+- [x] A user can mark a card that they have mastered and have it removed from the pool of answers as well as added to a list of mastered cards
 
 The following **additional** features are implemented:
 
-- [ ] List anything else that you added to improve the site's functionality!
+- [x] The input style and value changes when next and before button clicked
 
 ## Video Walkthrough
 
-Here's a walkthrough of implemented required features:
+Here's a walkthrough of implemented user stories:
 
-<img src='./flashCard.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
-
-https://imgur.com/a/NvoHZwc
-
+<img src='https://imgur.com/a/8bZRWSY' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+<img src = "./flashCard2.gif">
 <!-- Replace this with whatever GIF tool you used! -->
 
 GIF created with Kap
@@ -62,5 +60,3 @@ Describe any challenges encountered while building the app.
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License.
-
-# FlipCard
--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,91 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import FlashCards from "./components/flashCards";
 import cards from "./data/cards";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const App = () => {
+  const [masteredCards, setMasteredCards] = useState([]);
   const [flip, setFlip] = useState(false);
   const [whichCard, setWhichCard] = useState(0);
+  const [guess, setGuess] = useState("");
+  const [inputStyle, setInputStyle] = useState("");
+  const [streak, setStreak] = useState(0);
+  const [longestStreak, setLongestStreak] = useState(0);
+
   const pickQuestion = () => {
-    const newRandomNumber = Math.floor(Math.random() * cards.length);
-    setWhichCard(newRandomNumber);
+    setInputStyle("");
+    setGuess("");
+    if (whichCard < cards.length - 1) {
+      setWhichCard(whichCard + 1);
+    } else {
+      setWhichCard(0);
+    }
     setFlip(false);
   };
+  const shuffle = () => {
+    setGuess("");
+    setInputStyle("");
+    for (var i = cards.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+
+      var temp = cards[i];
+      cards[i] = cards[j];
+      cards[j] = temp;
+    }
+  };
+
+  const beforeQuestion = () => {
+    if (whichCard > 0) {
+      setWhichCard(whichCard - 1);
+    } else {
+      setWhichCard(cards.length - 1);
+    }
+    setFlip(false);
+  };
+
+  const handleChange = (e) => {
+    setGuess(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const updatedGuess = guess.toLowerCase().replaceAll(" ", "");
+    const boomCards = cards[whichCard].answer.split(" ");
+    const updatedCardsFirstName = boomCards[0].toLowerCase();
+    const updatedCardsLastName = boomCards[1].toLowerCase();
+    if (updatedCardsFirstName == updatedGuess) {
+      setInputStyle("rightStyle");
+      setStreak(streak + 1);
+    } else if (updatedCardsLastName == updatedGuess) {
+      setInputStyle("rightStyle");
+      setStreak(streak + 1);
+    } else {
+      setInputStyle("wrongStyle");
+      if (streak >= longestStreak) {
+        setLongestStreak(streak);
+      }
+      setStreak(0);
+    }
+  };
+
   const checkFlip = () => {
     setFlip(!flip);
+    setInputStyle("");
   };
+  const mastered = () => {
+    console.log(cards);
+    setMasteredCards([...masteredCards, cards[whichCard]]);
+
+    cards.splice(whichCard, 1);
+
+    if (whichCard >= cards.length) {
+      setWhichCard(0);
+    } else {
+      setWhichCard(whichCard);
+    }
+
+    setFlip(false);
+  };
+  console.log(masteredCards);
   return (
     <div className="container">
       <div className="header">
@@ -24,20 +97,53 @@ const App = () => {
           How good of a fan are you? Test all of your celebrity knowledge here!
         </h2>
         <div className="numberOfCards">Number of cards: {cards.length}</div>
+        <div className="streak">
+          <div className="numberOfCards">Current Streak: {streak}</div>
+          <div className="numberOfCards">Longest Streak: {longestStreak}</div>
+        </div>
       </div>
       <div className="content">
         <div className="cards" onClick={checkFlip}>
-          <FlashCards
-            isFlipped={flip}
-            question={cards[whichCard].question}
-            answer={cards[whichCard].answer}
-            image={cards[whichCard].image}
-            difficulty={cards[whichCard].difficulty}
-          />
+          {cards.length > 0 ? (
+            <FlashCards
+              isFlipped={flip}
+              question={cards[whichCard].question}
+              answer={cards[whichCard].answer}
+              image={cards[whichCard].image}
+              difficulty={cards[whichCard].difficulty}
+            />
+          ) : (
+            <h1>no more cards</h1>
+          )}
         </div>
-        <button className="nextButton" onClick={pickQuestion}>
-          <FontAwesomeIcon icon={faArrowRight} />
+        <button className="shuffleButton" onClick={mastered}>
+          Mastered
         </button>
+        <form className="guess" onSubmit={handleSubmit}>
+          <div className="containerInput">
+            <h1 className="inputText">Guess the name:</h1>
+            <input
+              className={inputStyle}
+              type="text"
+              value={guess}
+              onChange={handleChange}
+            />
+          </div>
+          <button className="submitButton" type="Submit">
+            Submit
+          </button>
+        </form>
+        <div className="buttons">
+          <button className="nextButton" onClick={beforeQuestion}>
+            <FontAwesomeIcon icon={faArrowLeft} />
+          </button>
+          <button className="nextButton" onClick={pickQuestion}>
+            <FontAwesomeIcon icon={faArrowRight} />
+          </button>
+          <button className="shuffleButton" onClick={shuffle}>
+            Shuffle Button
+          </button>
+        </div>
       </div>
     </div>
   );
